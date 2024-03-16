69dabf983c475a12274921bf68ffb445b5aa8fa6
FABIS8844 <160301415+FABIS8844@users.noreply.github.com>
2024-03-16 15:04:40 +0530

    fabis

--- lib/reg.dart
+++ lib/reg.dart
@@ -1,13 +1,54 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart'as http;
+
+import 'login.dart';
 
 class registration extends StatefulWidget {
   const registration({Key? key}) : super(key: key);
+
+
 
   @override
   State<registration> createState() => _registrationState();
 }
 
 class _registrationState extends State<registration> {
+  final TextEditingController _name=TextEditingController();
+  final TextEditingController _dob=TextEditingController();
+  final TextEditingController _username=TextEditingController();
+  final TextEditingController _password=TextEditingController();
+
+
+  bool loading=false;
+  void registration()async{
+    Uri uri=Uri.parse('https://scnner-web.onrender.com/api/register');
+    var response=await http.post(uri,
+    headers: <String,String>{
+      'content-type':'application/jason,charset=UTF-8',
+        },
+        body:jsonEncode({
+        'name':_name.text,
+        'dob':_name.text,
+        'username':_username.text,
+        'password':_password.text,
+        }));
+       if(response.statusCode==200){
+         Navigator.push(context, MaterialPageRoute(builder: (Context)=>Login()));
+
+       }
+       else{
+         ScaffoldMessenger.of(context).showSnackBar(SnackBar(content:Text('error'),));
+
+       }
+        print(_name.text);
+        print(_dob.text);
+        print(_username.text);
+        print(_password.text);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(backgroundColor: Colors.white,
@@ -19,7 +60,7 @@ class _registrationState extends State<registration> {
             children: [
               Padding(
                 padding: const EdgeInsets.all(20.0),
-                child: TextField(decoration: InputDecoration(
+                child: TextField(controller:_name,decoration: InputDecoration(
                     border: OutlineInputBorder(),
                     labelText: 'Enter Name',
                     hintText: ''
@@ -29,7 +70,7 @@ class _registrationState extends State<registration> {
                 height: 20,),
               Padding(
                 padding: const EdgeInsets.all(20.0),
-                child: TextField(decoration: InputDecoration(
+                child: TextField(controller:_dob,decoration: InputDecoration(
                     border: OutlineInputBorder(),
                     labelText: 'Enter DOB',
                     hintText: ''
@@ -39,7 +80,7 @@ class _registrationState extends State<registration> {
                 height: 20,),
               Padding(
                 padding: const EdgeInsets.all(20.0),
-                child: TextField(decoration: InputDecoration(
+                child: TextField(controller:_username,decoration: InputDecoration(
                     border: OutlineInputBorder(),
                     labelText: 'Username',
                     hintText: ''
@@ -49,7 +90,7 @@ class _registrationState extends State<registration> {
                 height: 20,),
               Padding(
                 padding: const EdgeInsets.all(20.0),
-                child: TextField(decoration: InputDecoration(
+                child: TextField(controller:_password,decoration: InputDecoration(
                     border: OutlineInputBorder(),
                     labelText: 'Password',
                     hintText: ''
@@ -57,7 +98,7 @@ class _registrationState extends State<registration> {
               ),
               SizedBox(
                 height: 20,),
-              TextButton(onPressed: null, child: Text('Register',style: TextStyle(color: Colors.greenAccent),))
+              TextButton(onPressed: registration, child: Text('Register',style: TextStyle(color: Colors.greenAccent),))
 
             ],),
         )
